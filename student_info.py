b331949fdeefa50119335380e4185e004039f436
veykiddo <harveyed.fajardo07@gmail.com>
2026-09-19 21:55:34 +0800

    Display student ID in student information

--- student_info.py
+++ student_info.py
@@ -1,8 +1,10 @@
 name = "Ed"
 course = "Computer Science"
 year_level = 3
+student_id = "2026-0001"
 
 print("=== STUDENT INFORMATION ===")
 print("Name:", name)
+print("Student ID:", student_id)
 print("Course:", course)
 print("Year Level:", year_level)
