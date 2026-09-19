57268a27a56a3e623c2f8f7f7d4b6abd115fef96
edaddie <edharvey.fajardo007@gmail.com>
2026-09-19 22:06:21 +0800

    Add Welcome Message

--- student_info.py
+++ student_info.py
@@ -3,8 +3,13 @@ course = "Computer Science"
 year_level = 3
 student_id = "2026-0001"
 
-print("=== STUDENT INFORMATION ===")
-print("Name:", name)
-print("Student ID:", student_id)
-print("Course:", course)
-print("Year Level:", year_level)
+def display_welcome():
+    print("\nWelcome to the Student Information System!")
+
+    print("=== STUDENT INFORMATION ===")
+    print("Name:", name)
+    print("Student ID:", student_id)
+    print("Course:", course)
+    print("Year Level:", year_level)
+
+display_welcome()
